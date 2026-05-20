a023173aed7832d66f15bcaaaaaf19bf000e3731
A.R.O.N.A <liyunhan11111@163.com>
2026-05-20 23:20:00 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  base: './',
+  optimizeDeps: {
+    include: ['marked']  // 显式包含 marked
+  },
+  build: {
+    rollupOptions: {
+      external: []  // 确保 marked 不被排除
+    }
+  }
 })
 
