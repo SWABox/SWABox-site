1854e158a2e1dbd9dc991bfd367ddcb7ab07b9d1
A.R.O.N.A <liyunhan11111@163.com>
2026-05-20 23:49:07 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,15 @@ import Features from './components/Features.vue'
 import TechStack from './components/TechStack.vue'
 import DownloadSection from './components/DownloadSection.vue'
 import SiteFooter from './components/SiteFooter.vue'
-import DocsView from './views/DocsView.vue'
-import {onMounted} from "vue";
-
-const isDocsPage = ref(false)
-
-onMounted(() => {
-  isDocsPage.value = window.location.pathname.includes('/docs')
-})
 </script>
 
 <template>
-  <div v-if="isDocsPage">
-    <DocsView />
-  </div>
-  <div v-else>
-    <NavBar />
-    <main>
-      <HeroSection />
-      <Features />
-      <TechStack />
-      <DownloadSection />
-    </main>
-    <SiteFooter />
-  </div>
+  <NavBar />
+  <main>
+    <HeroSection />
+    <Features />
+    <TechStack />
+    <DownloadSection />
+  </main>
+  <SiteFooter />
 </template>
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
-  optimizeDeps: {
-    include: ['marked']  // 显式包含 marked
-  },
-  build: {
-    rollupOptions: {
-      external: []  // 确保 marked 不被排除
-    }
-  }
+  base: './'
 })
 
