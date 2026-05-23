26c9f76aa7078fcdf31d2d0b8bec8eed142cf902
A.R.O.N.A <liyunhan11111@163.com>
2026-05-23 22:54:17 +0800

    更新

--- src/components/DownloadSection.vue
+++ src/components/DownloadSection.vue
@@ -99,7 +99,7 @@ onUnmounted(() => {
                 <polyline points="7 10 12 15 17 10"/>
                 <line x1="12" y1="15" x2="12" y2="3"/>
               </svg>
-              下载 v{{ release.version }}
+              下载 {{ release.version }}
             </a>
           </div>
 
